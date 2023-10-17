672ff82880d9b2d01c4d6d7f11807f30d1bf2d7d
mpc6332 <127924201+mpc6332@users.noreply.github.com>
2023-10-17 12:02:08 -0400

    Product updates

--- product_script.js
+++ product_script.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    // Initialize an object to store the form data as a JSON object
+    var formData = {};
+
     // Function to check if all required fields are filled out
     function areRequiredFieldsFilled() {
         var productId = $('#productId').val();
@@ -14,6 +17,17 @@ $(document).ready(function() {
     $('#additem-button').click(function(event) {
         // Check if all required fields are filled
         if (areRequiredFieldsFilled()) {
+
+            // Store the form data in the formData object as a JSON object
+            formData = {
+                productId: $('#productId').val(),
+                productDesc: $('#productDesc').val(),
+                productCategory: $('#productCategory').val(),
+                productUOM: $('#productUOM').val(),
+                productPrice: $('#productPrice').val(),
+                productWeight:$('#productWeight').val()
+            };
+
             // Hide the entire form and product management header
             $('form, .panel-title').hide();
             // Show the lookup section
@@ -23,6 +37,21 @@ $(document).ready(function() {
         } else {
             alert('Please fill out all required fields before activating the lookup section.');
             event.preventDefault(); // Prevent form submission if fields are not filled
+        }
+    });
+
+    // Function to handle the lookup search
+    $('#searchButton').click(function() {
+        var searchTerm = $('#searchProductId').val();
+
+        // Check if the search term matches the productId
+        if (formData.productId && formData.productId === searchTerm) {
+            var jsonData = JSON.stringify(formData, null, 2);
+            // Display the "JSON" header and the JSON data
+            $('#searchResult').html('<h3>JSON</h3><pre>' + jsonData + '</pre>');
+        } else {
+            // Display a message indicating no match
+            $('#searchResult').text('No match found.');
         }
     });
 });
