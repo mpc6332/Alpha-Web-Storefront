6d2173e349bc43118e3dba4c8f5017185cd94b17
mpc6332 <127924201+mpc6332@users.noreply.github.com>
2023-10-23 16:24:36 -0400

    Updated shopping cart files

--- shoppingcart.js
+++ shoppingcart.js
@@ -1,64 +1,76 @@
-// Sample JSON product data
-const products = [
-    {
-        id: 1,
-        name: "Bike Green",
-        price: 10,
-        image: "bike1.png"
-    },
-    {
-        id: 2,
-        name: "Bike Pink",
-        price: 15,
-        image: "bike2.png"
-    },
-    // Add more product objects as needed
-];
+// JSON Product information
+var products = {
+    "001": { name: "Men's Jeans", price: 59.99 },
+    "004": { name: "Women's T-Shirt", price: 19.99 },
+};
 
-// Function to add a product to the cart
-function addToCart(product) {
-    const cartItems = document.getElementById("cart-items");
-    const cartItem = document.createElement("div");
-    cartItem.classList.add("cart-item");
+// Add item to the cart or update quantity and total if it already exists
+document.querySelector("#addButton").addEventListener("click", function() {
+    var select = document.getElementById("dropdownInput");
+    var selectedOption = select.options[select.selectedIndex];
+    if (selectedOption.value !== "") {
+        var productId = selectedOption.value;
+        var productInfo = products[productId];
 
-    const itemImage = document.createElement("img");
-    itemImage.src = product.image;
+        // Get the cart table body
+        var table = document.querySelector(".table").getElementsByTagName("tbody")[0];
 
-    const itemName = document.createElement("span");
-    itemName.textContent = product.name;
+        // Check if the product is already in the table
+        var existingRow = Array.from(table.rows).find(function(row) {
+            return row.cells[0].textContent === productId;
+        });
 
-    const itemPrice = document.createElement("span");
-    itemPrice.textContent = "$" + product.price;
+        if (existingRow) {
+            // If the product is already in the table, update its quantity and total
+            var quantityCell = existingRow.cells[3].querySelector("input");
+            quantityCell.value = parseInt(quantityCell.value) + 1;
+            updateTotal(quantityCell);
+        } else {
+            // Add the product to the table
+            var newRow = table.insertRow(table.rows.length);
+            var productIdCell = newRow.insertCell(0);
+            var productNameCell = newRow.insertCell(1);
+            var priceCell = newRow.insertCell(2);
+            var quantityCell = newRow.insertCell(3);
+            var totalCell = newRow.insertCell(4);
+            var actionCell = newRow.insertCell(5);
 
-    cartItem.appendChild(itemImage);
-    cartItem.appendChild(itemName);
-    cartItem.appendChild(itemPrice);
-
-    cartItems.appendChild(cartItem);
-}
-
-// Function to calculate and update the total amount
-function updateTotal() {
-    const cartItems = document.getElementsByClassName("cart-item");
-    let total = 0;
-
-    for (let i = 0; i < cartItems.length; i++) {
-        const itemPrice = parseFloat(cartItems[i].querySelector("span:last-child").textContent.slice(1));
-        total += itemPrice;
+            productIdCell.innerHTML = productId;
+            productNameCell.innerHTML = productInfo.name;
+            priceCell.innerHTML = '$' + productInfo.price.toFixed(2);
+            quantityCell.innerHTML = '<input type="number" value="1" min="1" onchange="updateTotal(this)">';
+            totalCell.innerHTML = '$' + productInfo.price.toFixed(2);
+            actionCell.innerHTML = '<button class="btn btn-danger" onclick="removeRow(this)">Remove</button>';
+        }
     }
-
-    document.getElementById("total-amount").textContent = total.toFixed(2);
-}
-
-// Event listener for the checkout button
-document.getElementById("checkout-btn").addEventListener("click", function () {
-    // Add your checkout logic here
-    alert("Checkout button clicked!");
 });
 
-// Sample usage: Adding products to the cart
-addToCart(products[0]);
-addToCart(products[1]);
+// Remove item from the cart
+function removeRow(button) {
+    var row = button.parentNode.parentNode;
+    var productId = row.cells[0].textContent;
+    row.parentNode.removeChild(row);
+}
 
-// Sample usage: Updating the total amount
-updateTotal();
+// Update total based on quantity
+function updateTotal(input) {
+    var row = input.parentNode.parentNode;
+    var priceCell = row.cells[2];
+    var totalCell = row.cells[4];
+    var price = parseFloat(priceCell.innerHTML.replace('$', ''));
+    var quantity = parseInt(input.value);
+    var total = price * quantity;
+    totalCell.innerHTML = '$' + total.toFixed(2);
+    updateCartTotal();
+}
+
+// Update the cart total
+function updateCartTotal() {
+    var totalAmount = 0;
+    var table = document.querySelector(".table").getElementsByTagName("tbody")[0].rows;
+    for (var i = 0; i < table.length; i++) {
+        var totalCell = table[i].cells[4];
+        totalAmount += parseFloat(totalCell.innerHTML.replace('$', ''));
+    }
+    document.getElementById("total-amount").textContent = totalAmount.toFixed(2);
+}
