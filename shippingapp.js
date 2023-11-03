b54f48c7d5a6797a67d31d907c2d9a1e2ab36d57
Matthew Morrison <mlm6973@psu.edu>
2023-11-03 16:17:21 -0400

    Commit by M.M

--- shippingapp.js
+++ shippingapp.js
@@ -1,11 +1,17 @@
 angular.module('shippingapp', [])
     .controller('ShippingController', function ($scope) {
         $scope.displayJSON = function (){
-            if ($scope.address){
+            if ($scope.address,city,state,zip,carrierName,shippingMethod){
                 $scope.shippingInfoJSON = {
-                    address: $scope.address
+                    address: $scope.address,
+                    city: $scope.city,
+                    state: $scope.state,
+                    zip: $scope.zip,
+                    carrierName: $scope.carrierName,
+                    shippingMethod: $scope.shippingMethod
                 };
                 $scope.showJSON = true;
             }
+
         };
 });
